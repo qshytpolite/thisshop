6dff2ed9ef064b1b9c2e1fcb687230f61c5a17b2
Wachanga <31168616+qshytpolite@users.noreply.github.com>
2024-05-11 04:40:21 +0300

    second commit
    added templates. 
    user app

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # local apps
-    'sokotu',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom user settings
+AUTH_USER_MODEL = 'user.User'
---
+++ ecomm/user/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+from .form import CustomUserForm
+
+
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "Logged out Successfully")
+    return redirect("/")
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+    else:
+        if request.method == 'POST':
+            name = request.POST.get('username')
+            pwd = request.POST.get('password')
+            user = authenticate(request, username=name, password=pwd)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Logged in Successfully")
+                return redirect("/")
+            else:
+                messages.error(request, "Invalid User Name or Password")
+                return redirect("/login")
+        return render(request, "auths/login.html")
+
+
+def register(request):
+    form = CustomUserForm()
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "Registration Success You can Login Now..!")
+            return redirect('/login')
+    return render(request, "auths/register.html", {'form': form})
