8656ffec8be6d5a386765ad4d018c31d9e856dfc
Wachanga <31168616+qshytpolite@users.noreply.github.com>
2024-05-13 04:18:18 +0300

    Delete ecomm directory

--- ecomm/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_page, name="login"),
-    path('register/', views.register, name="register"),
-    path('logout/', views.logout_page, name="logout"),
-]
