88269c6ab7afaf8d451f1ca8c0df2834b6bb5580
qshytpolite <shiftdata.ke@gmail.com>
2024-06-09 00:30:56 +0300

    update settings

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['web-production-7ba5.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'thisshop.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['https://thisshop.up.railway.app']
 
 
 # Application definition
